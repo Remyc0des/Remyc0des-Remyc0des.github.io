9306d244f001abbd9e50298e7f26fa2539072d35
remonecarter1025 <remonecarter1025@gmail.com>
2025-06-18 06:05:40 +0000

    passwordchange

--- src/components/login.js
+++ src/components/login.js
@@ -24,7 +24,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app); 
 const db = getFirestore(app);
-const presetPassword = "NYCiSch00lprom25!"; 
+const presetPassword = "NYCiSch00lprom25"; 
 
 
 	document.addEventListener('DOMContentLoaded', () => {
